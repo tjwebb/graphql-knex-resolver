b2eb43a97f62c12757d5ca484930df09e6dd2abe
Travis Webb <me@traviswebb.com>
2016-02-24 20:38:37 -0800

    [api] setup api

---
+++ index.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const graphql = require('graphql')
+const lib = require('./lib')
+
+exports.Query = class Query {
+
+  /**
+   * Construct a new gql query
+   *
+   * @param gql String
+   */
+  constructor (gql) {
+    this.ast = graphql.parse(gql)
+  }
+
+  /**
+   * Construct a knex query
+   */
+  toKnexQuery (knex, args) {
+    const sql = lib.QueryBuilder.buildSQL(this.parsedQuery, knex)
+    return knex.raw(sql, args)
+  }
+}
